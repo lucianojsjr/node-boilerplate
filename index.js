930be81b816bb80f19c1065b21c0502c15f37d7e
Luciano José <luciano.jsjr@gmail.com>
2017-11-26 00:52:59 -0300

    Add Express Dependency and set User routes to API

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+const express = require('express');
 const http = require('http');
-const database = require('./api/database');
-
-const server = http.createServer();
 const port = process.env.PORT || 3000;
 
+const database = require('./api/database');
+const routes = require('./api/routes');
+
+let app;
+let server;
+
+const configApp = () => {
+	app = express();
+	app.use('/api', routes);
+};
+
 const startServer = () => {
+	server = http.createServer(app);
+
 	server.listen(port, () => {
 		console.log(`The Server is Running on Port: ${port}`);
 	});
@@ -14,7 +25,8 @@ const handleError = (error) => {
 	console.log(error);
 };
 
-database.sync()
+database.sequelize.sync()
+	.then(configApp)
 	.then(startServer)
 	.catch(handleError);
 
