b3c1091cbe7000f29d7bb4f5a165be945044b497
Luciano José <luciano.jsjr@gmail.com>
2017-11-26 00:50:36 -0300

    Fix Database Index Error

--- api/database.js
+++ api/database.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.json')[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, config.options);
-const db = {};
+
+let db = {};
 
 const readAll = (dir, filelist = []) => {
 	fs.readdirSync(dir).forEach(file => {
@@ -21,11 +22,9 @@ readAll(__dirname, [])
 		return file.indexOf('model.js') !== -1;
 	})
 	.forEach((file) => {
-		let model = require(file);
-		let instance = new model();
+		const model = sequelize.import(file);
 
-		sequelize.importCache[file] = instance.define(sequelize, Sequelize.DataTypes);
-		db[model.name] = sequelize.importCache[file];
+		db[model.name] = model;
 	});
 
 Object.keys(db).forEach(function(modelName) {
@@ -34,4 +33,7 @@ Object.keys(db).forEach(function(modelName) {
 	}
 });
 
-module.exports = sequelize;
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
